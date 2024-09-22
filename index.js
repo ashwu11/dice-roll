08b439f041137e82c7a3f14016261e3ed1598767
Ashley Wu <ashleywu2004@gmail.com>
2024-09-22 00:19:16 -0700

    implement change name

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const header = document.getElementsByTagName("h1")[0];
 const dice1 = document.getElementById("img1");
 const dice2 = document.getElementById("img2");
 const button = document.getElementsByTagName("button")[0];
+const p1name = document.getElementById("p1name");
+const p2name = document.getElementById("p2name");
+const input1 = document.getElementById("p1input");
+const input2 = document.getElementById("p2input");
 
 function rollDice() {
-    const number1 = Math.floor(Math.random() * 6) + 1;
-    const number2 = Math.floor(Math.random() * 6) + 1;
+    setPlayerNames()
+    
+    let number1 = Math.floor(Math.random() * 6) + 1;
+    let number2 = Math.floor(Math.random() * 6) + 1;
 
     dice1.setAttribute("src", "./images/dice" + number1 + ".png");
     dice2.setAttribute("src", "./images/dice" + number2 + ".png");
@@ -19,6 +25,11 @@ function rollDice() {
     }
 
     button.innerHTML = "Roll again!"
+}
+
+function setPlayerNames() {
+    if (input1.value != "") p1name.innerHTML = input1.value;
+    if (input2.value != "") p2name.innerHTML = input2.value;
 }
 
 button.addEventListener("click", rollDice)
