66bc247de81f8f4598af0e33dee3a1bf9167cc51
Ashley Wu <ashleywu2004@gmail.com>
2024-09-22 00:03:22 -0700

    add roll button functionality

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const header = document.getElementsByTagName("h1")[0];
 const dice1 = document.getElementById("img1");
 const dice2 = document.getElementById("img2");
+const button = document.getElementsByTagName("button")[0];
 
-const number1 = Math.floor(Math.random() * 6) + 1;
-const number2 = Math.floor(Math.random() * 6) + 1;
+function rollDice() {
+    const number1 = Math.floor(Math.random() * 6) + 1;
+    const number2 = Math.floor(Math.random() * 6) + 1;
 
-dice1.setAttribute("src", "./images/dice" + number1 + ".png");
-dice2.setAttribute("src", "./images/dice" + number2 + ".png");
+    dice1.setAttribute("src", "./images/dice" + number1 + ".png");
+    dice2.setAttribute("src", "./images/dice" + number2 + ".png");
 
-if (number1 > number2) {
-    header.innerHTML = "Player 1 wins!"
-} else if (number2 > number1) {
-    header.innerHTML = "Player 2 wins!"
-} else {
-    header.innerHTML = "Draw!"
+    if (number1 > number2) {
+        header.innerHTML = "Player 1 wins!"
+    } else if (number2 > number1) {
+        header.innerHTML = "Player 2 wins!"
+    } else {
+        header.innerHTML = "Draw!"
+    }
+
+    button.innerHTML = "Roll again!"
 }
+
+button.addEventListener("click", rollDice)
